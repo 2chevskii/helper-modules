c000d98b66bbf17b6c34849b76fc96bdc06c45c6
2chevskii <prsroman3@gmail.com>
2019-09-03 00:09:06 +0300

    Hope I got lang fixed, will test later

--- src/lang.ts
+++ src/lang.ts
@@ -1,68 +1,123 @@
 import * as fs from 'fs'
 import path from 'path'
 
-export class Lang {
-    private languages: Array<Array<string>>;
-    private serversettings: Array<string>;
-    private defaultlanguage: string;
-    constructor(deflocale: string, defmessages: Array<string>) {
-        this.languages = new Array<Array<string>>();
-        this.languages[deflocale] = defmessages
-        this.serversettings = new Array<string>()
-        this.defaultlanguage = deflocale
+const defaultlocale = 'en'
+const localesfolder = './locales'
+const serversettingspath = path.join(localesfolder, 'serversettings.json')
+const defaultlocalefile = path.join(localesfolder, `lang_${defaultlocale}.json`)
+
+interface LanguageData {
+    languages: Array<{}>
+    serversettings: Array<string>
+}
+
+export class LanguageHandler {
+    private data: LanguageData
+    constructor(defaultMessages: {}) {
+        this.data = {
+            languages: new Array<{}>(),
+            serversettings: new Array<string>()
+        }
+        this.loadSettings();
+        this.loadDefaultMessages(defaultMessages);
+        this.loadOtherLanguages();
     }
 
-    loadMessages(folder: string) {
-        if (!fs.existsSync(folder)) {
-            fs.mkdirSync(folder);
+    private loadSettings() {
+        if (!fs.existsSync(serversettingspath)) {
+            fs.writeFileSync(serversettingspath, JSON.stringify(this.data.serversettings, null, '\t'))
         }
 
-        var files = fs.readdirSync(folder);
-        let defname = `lang_${this.defaultlanguage}.json`;
-        let defaultexists = files.find((name) => name == defname)
-        if (!defaultexists) {
-            fs.writeFileSync(path.join(folder, defname), JSON.stringify(this.languages[this.defaultlanguage], null, '\t'))
+        try {
+            this.data.serversettings = JSON.parse(fs.readFileSync(serversettingspath, {
+                encoding: 'utf-8',
+                flag: 'r'
+            }))
+        } catch {
+            this.data.serversettings = new Array<string>();
+            fs.unlinkSync(serversettingspath);
+            this.loadSettings();
+        }
+    }
+
+    private loadDefaultMessages(defaultMessages: {}) {
+        if (!fs.existsSync(defaultlocalefile)) {
+            fs.writeFileSync(defaultlocalefile, JSON.stringify(defaultMessages))
         }
 
-        let langfiles = fs.readdirSync(folder).filter((name) => name.startsWith('lang_') && name.endsWith('.json'))
-        langfiles.forEach(file => {
-            let curlocale = file.replace('lang_', '').replace('.json', '');
-            try {
-                this.languages[curlocale] = JSON.parse(fs.readFileSync(path.join(folder, file), {
-                    encoding: 'utf-8',
-                    flag: 'r'
-                }))
-            } catch {
-                this.languages[curlocale] = this.languages[this.defaultlanguage]
-                fs.writeFileSync(path.join(folder, file), JSON.stringify(this.languages[curlocale], null, '\t'))
+        try {
+            this.data.languages[defaultlocale] = JSON.parse(fs.readFileSync(defaultlocalefile, {
+                encoding: 'utf-8',
+                flag: 'r'
+            }))
+        } catch {
+            fs.unlinkSync(defaultlocalefile);
+            this.loadDefaultMessages(defaultMessages);
+        }
+    }
+
+    private loadOtherLanguages() {
+        fs.readdirSync(localesfolder).forEach(file => {
+            if (file.startsWith('lang_') && file.endsWith('.json')) {
+                let locale = file.replace('lang_', '').replace('.json', '')
+                if (locale != defaultlocale) {
+                    try {
+                        this.data.languages[locale] = JSON.parse(fs.readFileSync(path.join(localesfolder, file), {
+                            encoding: 'utf-8',
+                            flag: 'r'
+                        }))
+                    } catch {
+                        fs.unlinkSync(path.join(localesfolder, file))
+                        console.log('Deleted ' + file + 'localization file as it does not contain valid json')
+                    }
+                }
             }
         });
     }
 
-    getMessage(id: string, key: string) {
-        return this.getMessagePrivate(this.getServerLanguage(id), key);
+    registerLanguage(locale: string, messages: {}) {
+        if (this.data.languages[locale] != undefined) {
+            return false;
+        }
+
+        this.data.languages[locale] = messages
+        fs.writeFileSync(path.join(localesfolder, `lang_${locale}.json`), JSON.stringify(this.data.languages[locale]))
+        return true;
     }
 
-    private getMessagePrivate(locale: string, key: string) {
-        return this.languages[locale][key];
+    unregisterLanguage(locale: string) {
+        if (this.data.languages[locale] == undefined) {
+            return false;
+        }
+
+        if (locale == defaultlocale) {
+            return false;
+        }
+
+        this.data.languages[locale] = undefined;
+        fs.unlinkSync(path.join(localesfolder, `lang_${locale}.json`))
+        return true;
     }
 
     setServerLanguage(id: string, locale: string) {
-        this.serversettings[id] = locale
+        this.data.serversettings[id] = locale;
     }
 
     getServerLanguage(id: string) {
-        const serverlang = this.serversettings[id];
-        if (serverlang == undefined) {
-            this.serversettings[id] = this.defaultlanguage;
-            return this.defaultlanguage;
+        let lang = this.data.serversettings[id];
+        if (lang == undefined) {
+            lang = defaultlocale;
+            this.setServerLanguage(id, lang);
         }
-        return serverlang;
+        return lang;
     }
 
-    getAvailableLanguages() {
-        return Object.keys(this.languages);
+    getMessage(id: string, key: string) {
+        let locale = this.getServerLanguage(id);
+        return this.getMessageLocale(locale, key);
+    }
+
+    private getMessageLocale(locale: string, key: string) {
+        return this.data.languages[locale][key];
     }
 }
-
-export default { Lang }
