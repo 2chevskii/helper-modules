54f1510d0ef0c79cd35b84d6aa3b11ef1b443402
2chevskii <prsroman3@gmail.com>
2019-10-03 17:37:13 +0300

    module file added, not sure about the best option for it yet

---
+++ src/config-module.ts
@@ -0,0 +1,23 @@
+// import * as path from 'path'
+// import * as fs from 'fs'
+// import { Log } from './log-module'
+
+// export namespace Config {
+//     const configfile = `.${path.sep}helper-modules${path.sep}config-module.json`
+
+    
+
+//     class ConfigInternal {
+//         private static _instance: ConfigInternal
+        
+
+//         public static get Instance(): ConfigInternal {
+//             if (ConfigInternal._instance === undefined) {
+//                 ConfigInternal._instance = new ConfigInternal()
+//             }
+//             return ConfigInternal._instance;
+//         }
+//     }
+
+
+// }
